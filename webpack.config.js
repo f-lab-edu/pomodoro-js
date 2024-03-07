a9fa23b44f4573fe4b7f780b96bbc7efe57a1aff
ckhe1215 <ckhe1215@gmail.com>
2024-03-08 00:54:34 +0900

    :bricks: Install: webpack dev tool, HMR, HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,22 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
 export default {
   mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
   },
+  devServer: {
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/templates/header.html",
+      filename: "./header.html",
+    }),
+  ],
 };
