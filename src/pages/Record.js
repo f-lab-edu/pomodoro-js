4e14353de2da493ca7e9c3a0765491692f16bbe2
haeun kim <ckhe1215@gmail.com>
2024-03-16 22:13:51 +0900

    :sparkles: Feat: path param 가져와서 데이터 업데이트 기능 추가

--- src/pages/Record.js
+++ src/pages/Record.js
@@ -7,6 +7,18 @@ class Record extends Component {
       router.navigate("/");
     });
   }
+  mount() {
+    const pTag = document.querySelector(".date_info");
+    const { date } = router.getParams();
+    const today = new Date();
+    if (date) {
+      pTag.innerText = date;
+    } else {
+      const mm = String(today.getMonth() + 1).padStart(2, "0");
+      const dd = String(today.getDate()).padStart(2, "0");
+      pTag.innerText = mm + dd;
+    }
+  }
 }
 
 export default Record;
